e8aecf4a2821d37cf2387d984bb6ebdc27e88b7e
Anqi Qu <anqiqu@gmail.com>
2026-06-13 22:11:16 +0100

    Basic SEO stuff

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import { installGoogleAnalytics } from "./analytics";
 import { ContextGatheringApp } from "./context-gathering/ContextGatheringApp";
 import "./styles.css";
+
+installGoogleAnalytics(import.meta.env.VITE_GA_MEASUREMENT_ID);
 
 const root = document.getElementById("root");
 
